e34d33a5fe721da34db3b90c68a54ad80df5b088
bituzin <43650567+bituzin@users.noreply.github.com>
2025-10-22 23:17:57 +0200

    Create CreatorOwnedToken.sol
    A self-allocating token contract that automatically rewards the deployer with initial tokens
    
    default token: Bituzin BTZ
    default amount: 1000

---
+++ CreatorOwnedToken.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract YourToken is ERC20 {
+    constructor() ERC20("Bituzin", "BTZ") {
+        _mint(msg.sender, 1000 * 10 ** 18); 
+    }
+}
